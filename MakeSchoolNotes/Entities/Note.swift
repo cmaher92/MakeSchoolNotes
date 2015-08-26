9afc806cf9919c3e6348d74e9282d04f8dbd2db5
Connor Maher <connormmaher@gmail.com>
2015-08-26 00:53:29 -0400

    Halfway through the note app, will need to study some of the TableView classes.

--- MakeSchoolNotes/Entities/Note.swift
+++ MakeSchoolNotes/Entities/Note.swift
@@ -10,6 +10,7 @@ import Foundation
 import RealmSwift
 
 class Note : Object {
-
-    
+    dynamic var title: String = ""
+    dynamic var content: String = ""
+    dynamic var modificationDate = NSDate()
 }
--- MakeSchoolNotes/ViewControllers/NotesViewController.swift
+++ MakeSchoolNotes/ViewControllers/NotesViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 import RealmSwift
 
 class NotesViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var notes: Results<Note>! {
+        didSet {
+            // Whenever notes update, update the table view
+            tableView?.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        let myNote = Note()
+        myNote.title   = "Super Simple Test Note"
+        myNote.content = "A long piece of content"
+        let realm = Realm() // 1
+        realm.write() { // 2
+            realm.add(myNote) // 3
+        }
+        notes = realm.objects(Note)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +38,23 @@ class NotesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension NotesViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("NoteCell", forIndexPath: indexPath) as! NoteTableViewCell //1
+        
+        let row = indexPath.row
+        let note = notes[row] as Note
+        cell.note = note
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Int(notes?.count ?? 0)
+    }
+    
 }
 
